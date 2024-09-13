619c329a5aea4f9ac2c172596e72c0e387987b96
Sreedharreddykasam <54680394+Sreedharreddykasam@users.noreply.github.com>
2024-09-13 21:24:20 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,5 +1,5 @@
 import pytest
-from app import app
+#from app import app
 
 @pytest.fixture
 def client():
@@ -13,11 +13,11 @@ def test_home(client):
     assert response.json == {'version': '1.0'}
 
 # Fahrenheit to Celsius
-#@pytest.mark.parametrize("temp, expected", [(212, 100.0), (32, 0.0), (-40, -40.0)])
-#def test_convert_temp_f_to_c(client, temp, expected):
-#    response = client.get(f'/convert-temp?temp={temp}&scale=fahrenheit&target_scale=celsius')
- #   assert response.status_code == 200
-  #  assert response.json['converted_temp'] == pytest.approx(expected, rel=1e-2)
+@pytest.mark.parametrize("temp, expected", [(212, 100.0), (32, 0.0), (-40, -40.0)])
+def test_convert_temp_f_to_c(client, temp, expected):
+    response = client.get(f'/convert-temp?temp={temp}&scale=fahrenheit&target_scale=celsius')
+    assert response.status_code == 200
+    assert response.json['converted_temp'] == pytest.approx(expected, rel=1e-2)
 
 # Celsius to Fahrenheit
 @pytest.mark.parametrize("temp, expected", [(100, 212.0), (0, 32.0), (-40, -40.0)])
